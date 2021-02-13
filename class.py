ace5600721782eebb402d74ba8981c5cc2050527
Ankit Sangwan <78931195+ankit21-spec@users.noreply.github.com>
2021-02-13 09:53:03 +0530

    Create class.py

---
+++ class.py
@@ -0,0 +1,13 @@
+def computer():
+    def __init__(self):
+        self.cpu = cpu
+        self.ram = ram
+    
+    def config(self):
+        print("Config is:", self.cpu, self.ram)
+        
+com1 = computer('i7', 16)
+com2 = computer('Ryzen 5', 8)
+
+com1.config()
+com2.config()
